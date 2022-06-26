04093b7c4621bcdc98d74b60efeefaee7f45c785
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-06-26 18:10:43 +0400

    v3.0.15

--- dist/config.js
+++ dist/config.js
@@ -37,7 +37,7 @@ const config = {
     'settings': {
         'node': {
             'allowModules': ['electron', 'svelte'],
-            'tryExtensions': ['.js', '.ts', '.node'],
+            'tryExtensions': ['.js', '.ts'],
         },
     },
     overrides: [
